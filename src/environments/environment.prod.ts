d068361874c399fd8b7c6249b3116ee1d9761d09
Jhonnanthn Balsas <jhoonasw@hotmail.com.br>
2026-08-14 20:02:21 -0300

    fix: apiUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.scaliz.com.br',
+  apiUrl: 'https://api.scaliz.com.br/v1',
   marketplaceId: 'ATVPDKIKX0DER',
   useMock: false
 };
