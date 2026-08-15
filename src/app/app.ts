5581faeeee8cb61af659899baa7c3ce52897d465
Jhonnanthn Balsas <jhoonasw@hotmail.com.br>
2026-08-15 18:57:57 -0300

    fix: versão de dashboard

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,21 +1,12 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('scaliz-shell');
-  protected readonly sidebarOpen = signal(false);
-
-  protected toggleSidebar(): void {
-    this.sidebarOpen.update((open) => !open);
-  }
-
-  protected closeSidebar(): void {
-    this.sidebarOpen.set(false);
-  }
+  protected readonly title = 'scaliz-shell';
 }
